ff91cfec79a74dc1d106a462eb7ffbb7a07083e7
Tiso <tisobyn@gmail.com>
2022-03-24 21:11:43 +0600

    fixed bug

--- Wordle/Scene/ViewController.swift
+++ Wordle/Scene/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGray6
+        view.backgroundColor = .brown
 
     }
 
